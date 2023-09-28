45740dfe9575161e77b6b54e4f41523cec158877
Ajay Singh <ajay030320@gmail.com>
2023-09-27 23:49:33 -0700

    book_shop

---
+++ inc/contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="process_contact.php" method="POST">
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name" required>
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" required>
+
+    <label for="message">Message:</label>
+    <textarea name="message" id="message" required></textarea>
+
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
